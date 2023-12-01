e0095a78ff73ff4319b8bf694a8014e1879771df
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2023-12-01 23:13:22 +0100

    Trying to make day1 more readable

--- src/main/kotlin/days/day01.kt
+++ src/main/kotlin/days/day01.kt
@@ -2,56 +2,68 @@ package days
 
 import utils.Puzzle
 
-val day01pt1 = Puzzle { input ->
+val day01pt1 = Puzzle(
+    day = 1,
+    part = 1,
+) { input ->
     val lines = input.lines()
-    val numbers = lines.map { line ->
+    val calibrationValues = lines.map { line ->
         val digits = line.filter { it.isDigit() }
         if (digits.isNotBlank()) {
             return@map "${digits.first()}${digits.last()}".toIntOrNull()
         }
         return@map null
     }.filterNotNull()
-    return@Puzzle numbers.sum()
+    return@Puzzle calibrationValues.sum()
 }
 
-val day01pt2 = Puzzle { input ->
-    val stringDigitMap = mapOf(
-        "zero" to '0',
-        "one" to '1',
-        "two" to '2',
-        "three" to '3',
-        "four" to '4',
-        "five" to '5',
-        "six" to '6',
-        "seven" to '7',
-        "eight" to '8',
-        "nine" to '9',
-    )
+val day01pt2 = Puzzle(
+    day = 1,
+    part = 2,
+) { input ->
 
-    val lines = input.lines()
+    fun getNumbersFromLine(line: String): List<Int> {
+        val stringDigitMap = mapOf(
+            "zero" to '0',
+            "one" to '1',
+            "two" to '2',
+            "three" to '3',
+            "four" to '4',
+            "five" to '5',
+            "six" to '6',
+            "seven" to '7',
+            "eight" to '8',
+            "nine" to '9',
+        )
 
-    val numberLines = lines.map { line ->
         val newLine = mutableListOf<Int>()
 
         line.forEachIndexed { index, c ->
-            if (c.isDigit()) newLine += c.toString().toInt()
+            if (c.isDigit()) {
+                newLine.add(c.toString().toInt())
+            }
             else {
-                val subLine = line.substring(index)
+                val substring = line.substring(index)
                 stringDigitMap.forEach { (letters, value) ->
-                    if (subLine.startsWith(letters)) {
-                        newLine += value.toString().toInt()
+                    if (substring.startsWith(letters)) {
+                        newLine.add(value.toString().toInt())
                     }
                 }
             }
         }
 
-        newLine
+        return newLine
     }
 
-    val numbers = numberLines.map { line ->
-        if (line.isEmpty()) return@map null
-        "${line.first()}${line.last()}".toIntOrNull()
-    }.filterNotNull()
+    val lines = input.lines()
 
-    return@Puzzle numbers.sum()
+    val numbers = lines.mapNotNull { line ->
+        return@mapNotNull getNumbersFromLine(line).ifEmpty { null }
+    }
+
+    val calibrationValues = numbers.mapNotNull { line ->
+        "${line.first()}${line.last()}".toIntOrNull()
+    }
+
+    return@Puzzle calibrationValues.sum()
 }
